cc4628647f6586c556c802c9177abacf909ace8e
Fogo Software <tuncaydemir682@gmail.com>
2023-07-08 17:23:45 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             <CategoryList title="Category List" />
           </Col>
           <Col xs="9">
-            <ProductList title="Category List" />
+            <ProductList title="Product List" />
           </Col>
         </Row>
       </Container>
