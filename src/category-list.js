7e0c206be8eff41e1bc4702af2e76e67b221acb0
Fogo Software <tuncaydemir682@gmail.com>
2023-07-08 16:56:41 +0300

    Updated

--- src/category-list.js
+++ src/category-list.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { ListGroup, ListGroupItem } from 'reactstrap'
+import { ListGroup, ListGroupItem, ListGroupItemHeading } from 'reactstrap'
 
 export default class CategoryList extends Component {
   render() {
     return (
-      <div>
-        <ListGroup>
+      <div className='my-5'>
+        <ListGroupItemHeading>Category List</ListGroupItemHeading>
+        <ListGroup className='my-4'>
           <ListGroupItem>Item 1</ListGroupItem>
           <ListGroupItem>Item 2</ListGroupItem>
           <ListGroupItem>Item 3</ListGroupItem>
