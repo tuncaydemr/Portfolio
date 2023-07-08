90d6566bf8b51ed378103b14671a10e74908e2b0
Fogo Software <tuncaydemir682@gmail.com>
2023-07-08 21:14:00 +0300

    Updated

--- src/category-list.js
+++ src/category-list.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
+  state = {
+    categories :
+    [
+      {categoryId : 1, categoryName : "Beverages"},
+      {categoryId : 2, categoryName : "Condiments"}
+    ],
+
+    currentCategory : ""
+  }
+
+  changeCategory = (category) => {
+    this.setState({currentCategory : category.categoryName})
+  }
+
   render() {
     return (
       <div className='my-5'>
         <h2>{this.props.info.title}</h2>
         <ListGroup className='my-4'>
-          <ListGroupItem>Item 1</ListGroupItem>
-          <ListGroupItem>Item 2</ListGroupItem>
-          <ListGroupItem>Item 3</ListGroupItem>
-          <ListGroupItem>Item 4</ListGroupItem>
+          {
+            this.state.categories.map(category => (
+              <ListGroupItem onClick={() => this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            ))
+          }
         </ListGroup>
+        <h3>{this.state.currentCategory}</h3>
       </div>
 
     )
