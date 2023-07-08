1070b8ece3177dd501781c66ac10aa5dee488724
Fogo Software <tuncaydemir682@gmail.com>
2023-07-08 17:11:59 +0300

    Updated

--- src/product-list.js
+++ src/product-list.js
@@ -5,7 +5,7 @@ export default class ProductList extends Component {
   render() {
     return (
       <div className='my-5'>
-        <ListGroupItemHeading>Product List</ListGroupItemHeading>
+        <h2>Product List</h2>
         <ListGroup className='my-4'>
           <ListGroupItem>Item 1</ListGroupItem>
           <ListGroupItem>Item 2</ListGroupItem>
