117340ae5cba549a23b4bf72fc118cb2a9b2e1df
Fogo Software <tuncaydemir682@gmail.com>
2023-07-08 17:11:44 +0300

    Updated

--- src/category-list.js
+++ src/category-list.js
@@ -5,7 +5,7 @@ export default class CategoryList extends Component {
   render() {
     return (
       <div className='my-5'>
-        <ListGroupItemHeading>Category List</ListGroupItemHeading>
+        <h2>Category List</h2>
         <ListGroup className='my-4'>
           <ListGroupItem>Item 1</ListGroupItem>
           <ListGroupItem>Item 2</ListGroupItem>
